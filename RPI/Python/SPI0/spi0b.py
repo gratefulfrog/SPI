61defcc6dcfa3be9c0df66fb92a7bac985ab6b1a
gratefulfrog <gratefulfrog@gmail.com>
2017-10-01 10:28:58 +0000

    working spi0b.py with iteaduino

--- RPI/Python/SPI0/spi0b.py
+++ RPI/Python/SPI0/spi0b.py
@@ -3,7 +3,7 @@
 # wiring of SPI 0,0:
 """
 Color  : Function : RPI pin (SPI0)  : Smartscope
-Yellow : SS       : 24              : D0         NOTE: RPI SPI0-CS1 is  RPI Pin 26
+Yellow : SS       : 24              : D0     NOTE: RPI SPI0-CS1 is  RPI Pin 26
 White  : SCK      : 23              : D1
 Blue   : MISO     : 21              : D3
 Orange : MOSI     : 19              : D2
@@ -12,47 +12,36 @@ Orange : MOSI     : 19              : D2
 import spidev,time, sys
 
 # time after each transfer to observe results
-pause   = 0  # 0.1 seconds
+pause   = 0.0   # 0.1 seconds
 # SPI config
 channel = 0
 device  = 0
 
-def go(chars):
+def go():
     spi = spidev.SpiDev()
     spi.open(channel,device)
-    # characters to send
-    # out = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     index = 0
     init = False
     sendCount=0
     try:
         while True:
-            if (chars):
-                pass
-                """
-                outgoing = out[index]
-                resp = spi.xfer([ord(outgoing)])  # xfer takes list of ints as arg
-                print (sendCount, ': ', [outgoing,  chr(resp[0])])   # and returns a list of ints
-                sendCount+=1
-                # check the reply w.r.t. previous send 
-                if (init and
-                    (chr(resp[0]) != out[len(out)-1 if index == 0 else index-1])):
-                    print('Error!')
-                    raise KeyboardInterrupt
-                index=(index+1)%len(out)
-                """
-            else:
-                resp = spi.xfer([index])
-                print (sendCount, ': ', [index]+ resp)
-                sendCount+=1
-                # check reply w.r.t. previous send
-                if (init and
-                    (resp[0] != (0 if index == 0 else 256-index))):
-                    print(index, 'Error!')
-                    raise KeyboardInterrupt
-                index=(index+1)%256
-            init = True
+            resp = spi.xfer([index],1000000, 2)
+            # xfer args: list of bytes,
+            #            Hz freq of clck,
+            #            uSec delay before releasing SS
             time.sleep(pause)
+            #if ((sendCount % 10000) == 0):
+            #    print (sendCount, ': ', [index]+ resp)
+            #print (sendCount, ': ', [index]+ resp)
+            sendCount+=1
+            # check reply w.r.t. previous send
+            if (init and
+                (resp[0] != (0 if index == 0 else 256-index))):
+                print(index, resp[0], 'Error!')
+                raise KeyboardInterrupt
+            #input()
+            index=(index+1)%256
+            init = True
     except KeyboardInterrupt:
         print('\nbye...')
     finally:
@@ -60,4 +49,4 @@ def go(chars):
 
 if __name__ == '__main__':
     # if arg given, then do letters, else do numbers!
-    go(False)
+    go()
