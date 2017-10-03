f520d9e7e9b1a7f4f660362cde388b4aa55d6220
gratefulfrog <gratefulfrog@gmail.com>
2017-10-03 06:34:40 +0000

    spi1b.py receives arbitrary number of bytes

--- RPI/Python/SPI1/spi1b.py
+++ RPI/Python/SPI1/spi1b.py
@@ -32,9 +32,10 @@ import spidev,time
 # time after each transfer to observe results
 pause   = 0.0000   # seconds
 # SPI config
-channel = 0
-device  = 0
-sendCount = 0
+channel        = 0
+device         = 0
+frequency      = 1000000
+afterXferDelay = 2
 
 def masterMsg(rightHalfByte):
     return [(0b1111 & rightHalfByte)<<4]
@@ -42,48 +43,50 @@ def masterMsg(rightHalfByte):
 def isMasterMsg(byte):
     return (byte & (0b1111<<4))
 
+def transferLis(outLis,spi):
+    # outLis should be the vector of outgoing bytes, not yet left shifted
+    # returns
+    # [lis of bytes received, after re-assembly,correctedCount]
+    responseLis = []
+    correctedCount = 0
+    # ignore first reply
+    spi.xfer(masterMsg(outLis[0]),frequency,afterXferDelay)
+    time.sleep(pause)
+    
+    for outByte in outLis[1:]:
+        responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
+        while(isMasterMsg(responseLis[-1])):
+            correctedCount +=1
+            print('Error Corrected :',correctedCount)
+            responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
+            # here we have a good value in r1
+        time.sleep(pause)
+    # here we have the response list filled with good values
+
+    resLis = []
+    for i in range(0, len(responseLis), 2):
+        resLis += [responseLis[i]<<4 | responseLis[i+1]]
+    return resLis,correctedCount
+
 def go():
     spi = spidev.SpiDev()
     spi.open(channel,device)
     outVec = [0b1000,1,2]
-    sendCount  = 0
+    transferCount  = 0
     errorCount = 0
     correctedCount = 0
     lastResponse = -1
-    currentRepsonse = 0
     init = False
     try:
         while True:
-            outIndex = 0
-            # send the type and ignore left over value of SPDR 
-            spi.xfer(masterMsg(outVec[outIndex]),1000000,2)
-            
-            time.sleep(pause)
-            outIndex +=1
-            [r1] = spi.xfer(masterMsg(outVec[outIndex]),1000000,2)
-            while(isMasterMsg(r1)):
-                correctedCount +=1
-                print('Error Corrected :',correctedCount)
-                [r1] = spi.xfer(masterMsg(outVec[outIndex]),1000000,2)
-            # here we have a good value in r1
-
-            time.sleep(pause)
-            outIndex +=1
-            [r2] = spi.xfer(masterMsg(outVec[outIndex]),1000000,2)
-            while(isMasterMsg(r2)):
-                correctedCount +=1
-                print('Error Corrected :',correctedCount)
-                [r2] = spi.xfer(masterMsg(outVec[outIndex]),1000000,2)
-            # here we have a good value in r2
-                  
-            time.sleep(pause)
-            sendCount+=1
-            currentResponse = r1<<4 | r2
+            [[currentResponse],correctedInc] = transferLis(outVec,spi)
+            transferCount+= 1  # note we are now counting transfers not bytes
+            correctedCount += correctedInc
             if not init:
                 lastResponse = 255 if currentResponse==0 else currentResponse-1
                 init = True;
-            if ((sendCount % 10000) == 0):
-                print(sendCount,
+            if ((transferCount % 10000) == 0):
+                print(transferCount,
                       ':',
                       currentResponse,
                       '  ' if  currentResponse<10 else
@@ -93,11 +96,11 @@ def go():
                       errorCount,
                       ': Corrected :',
                       correctedCount)
-
+            
             # check relative to previous
             if (currentResponse!= (lastResponse +1)%256):
                 errorCount+=1
-                print(sendCount,
+                print(transferCount,
                       ' : ',
                       [currentResponse,lastResponse],
                       'Error :',
@@ -105,13 +108,14 @@ def go():
                       '!********************')
                 #input()
             else:
-                lastResponse = r1<<4 | r2
+                
+                lastResponse = currentResponse
             
     except KeyboardInterrupt:
-        print('\nNb Sends  : ','{:,}'.format(sendCount).replace(',',' '))
-        print(  'Nb Errors : ',errorCount)
-        print(  'percent   : ',100*errorCount/sendCount)
-        print(  'Corrected : ',correctedCount)
+        print('\nNb Transfers  : ','{:,}'.format(transferCount).replace(',',' '))
+        print(  'Nb Errors     : ',errorCount)
+        print(  'percent       : ',100*errorCount/transferCount)
+        print(  'Corrected     : ',correctedCount)
         print('\nbye...')
     finally:
         spi.close()
