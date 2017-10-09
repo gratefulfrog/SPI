dfcb06577f519ddb4606126af6c5bb38f69f723e
gratefulfrog <gratefulfrog@gmail.com>
2017-10-09 14:51:59 +0200

    guid integrated

--- RPI/Python/test_Integration_1_states/Slave2650/board.h
+++ RPI/Python/test_Integration_1_states/Slave2650/board.h
@@ -2,13 +2,13 @@
 #define BOARD_H
 
 #include <Arduino.h>
-
+#include "guid_24AA025UID.h"
 #include "config.h"
 
 
 class Board{
   protected:
-    const boardID guid;    /*!< board id, as guid, needs to be redone when the real GUID functionality is available */
+    boardID guid;    /*!< board id, as guid, needs to be redone when the real GUID functionality is available */
     
   public:
     Board();  
