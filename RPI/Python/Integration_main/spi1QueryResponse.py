8d3a9ba46aa5dc5b151be6d4958359b79a68e7a4
gratefulfrog <gratefulfrog@gmail.com>
2017-10-09 15:01:47 +0000

    simplified state graph

--- RPI/Python/Integration_main/spi1QueryResponse.py
+++ RPI/Python/Integration_main/spi1QueryResponse.py
@@ -20,6 +20,7 @@ from struct import pack,unpack
 
 # time after each transfer to observe results
 pause   = 0.0000   # seconds
+
 # SPI config
 channel        = 0
 device         = 0
@@ -30,21 +31,14 @@ maxTestFloat = 10.0
 pollDisplayIterations = 100
 
 s_init_t      = 0b1000  # DEC  8
-s_bid_t       = 0b1001  # DEC  9
 s_payload_t   = 0b1011  # DEC 11
-
-#s_startWork_t = 0b1100  # DEC 12
-
-# state errors/ 1000 polls
-# 229 with 0.01 delays
-#
 
 typeDict = { # key=type : value=[ngBytes, formatString, wait time in seconds before next communciation]
     s_init_t       : [9,'<BIf',1], # type 1000, 9 bytes struct, wait after 0.1 s
-    s_bid_t        : [9,'<BIf',1], # type 1001, 9 bytes struct, wait after 0.1 s             
     s_payload_t    : [9,'<BIf',0.1]} # type 1010, 9 bytes struct, wait after for 0.1 s
 
-type2NameDict = dict((eval(name),name) for name in ['s_init_t','s_bid_t','s_payload_t'])
+type2NameDict = dict((eval(name),name) for name in ['s_init_t',
+                                                    's_payload_t'])
 
 nullResponse = [255,0,0.0]  # used as sentinel value
 
@@ -120,17 +114,138 @@ def getModV(type):
     elif (type == u32_t):
         modV = pow(2,32)
     return modV
+
+def masterMsg(rightHalfByte):
+    return [(0b1111 & rightHalfByte)<<4]
+
+def isMasterMsg(byte):
+    return (byte & (0b1111<<4))
+
+def transferLis(outLis,spi):
+    # outLis should be the vector of outgoing bytes, not yet left shifted
+    # returns
+    # [lis of bytes received, after re-assembly,correctedCount]
+    responseLis = []
+    correctedCount = 0
+    # ignore first reply
+    spi.xfer(masterMsg(outLis[0]),frequency,afterXferDelay)
+    time.sleep(pause)
     
-        
-def tell(init, transferCount,errorCount,currentResponseLis,lastResponseLis,type):
+    for outByte in outLis[1:]:
+        responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
+        while(isMasterMsg(responseLis[-1])):
+            correctedCount +=1
+            print('Error Corrected :',correctedCount)
+            responseLis = responseLis[:-1]
+            responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
+            # here we have a good value in r1
+        time.sleep(pause)
+    # here we have the response list filled with good values
+
+    resLis = []
+    for i in range(0, len(responseLis), 2):
+        resLis += [responseLis[i]<<4 | responseLis[i+1]]
+    return resLis,correctedCount
+
+def getBid(currentResponseLis):
+    return currentResponseLis[1]
+
+## temp value until init call!
+bid = [-1]
+
+def doOneCom(type,spi,q,clearTheAir=False,printResults=False):
+    global bid
+    #print('Processing Query :',type)
+    outVec = getOutVec(type,typeDict[type][0])
+    transferCount  = 0
+    errorCount = 0
+    correctedCount = 0
+    lastResponseLis = [] 
+    init = False
+    saidEnq = False
+    # first one is ignored, just to clear the air!
+    if clearTheAir:
+        pass
+    #transferLis(outVec,spi)
+    moreDataComing = True
+    try:
+        while moreDataComing:
+            #input()
+            [currentResponseLis,correctedInc] = transferLis(outVec,spi)
+            transferCount+= 1  # note we are now counting transfers not bytes
+            correctedCount += correctedInc
+
+            currentResponseLis = unpackStruct(typeDict[type][1],packNbytes(currentResponseLis))
+
+            if printResults:
+                show(transferCount,#errorCount,correctedCount,
+                     currentResponseLis,
+                     type)
+
+            nullReturn = isNullReturn(currentResponseLis)
+            enQResponse = (type == s_payload_t)
+            if nullReturn:
+                pass
+            elif enQResponse:
+                if not saidEnq:
+                    #print('***** Enqueueing...')
+                    saidEnq = True
+                q.put(list(currentResponseLis) + bid)
+            elif (type == s_init_t):
+                bid[0] = getBid(currentResponseLis)
+                print('BID set :', bid), 
+                
+            moreDataComing = not nullReturn and enQResponse
+                 
+    except KeyboardInterrupt:
+        print('\nType of Transfers :', type)
+        print(  'Nb Transfers      :','{:,}'.format(transferCount).replace(',',' '))
+        print(  'Nb Errors         :',errorCount)
+        print(  'percent           :',100*errorCount/transferCount)
+        print(  'Corrected         :',correctedCount)
+        print('\nbye...')
+        raise KeyboardInterrupt
+    #print('Sleeping :',typeDict[type][2])
+    time.sleep(typeDict[type][2])
+    
+
+def go(typeLis,q):
+    """ opens SPI
+    sends each elt in typeLis as per:
+    [0] : clearTheAir=True, printResults=False
+    loop indefinitely:
+      [1] : use default values for clearTheAir and printResults
+    on exit, closes spi
     """
+    spi = spidev.SpiDev()
+    spi.open(channel,device)
+    try:
+        print('Polling :',type2NameDict[typeLis[0]])
+        doOneCom(typeLis[0],spi,q,True,False)
+        #input()
+        count = 1
+        while True:
+            if (count%pollDisplayIterations == 0):
+                print(count, ': Polling :',type2NameDict[typeLis[1]])
+            count +=1                
+            doOneCom(typeLis[1],spi,q)
+    finally:
+        spi.close()
+
+
+## OBSOLETE CODE ##
+    
+"""
+cannot work with real data and query response        
+def tell(init, transferCount,errorCount,currentResponseLis,lastResponseLis,type):
+    
     type is one of:
        s_init_t    
        s_bid_t     
        s_payload_t 
        s_wakeup_t   
     return [init, errorCount, lastResponseLis]
-    """
+    
     tempLRL = lastResponseLis
     # the test below is no longer needed since now all responses are structs!
     #if type == s_payload_t:  
@@ -166,136 +281,5 @@ def tell(init, transferCount,errorCount,currentResponseLis,lastResponseLis,type)
     else:
         lastResponseLis = currentResponseLis
     return [init, errorCount,lastResponseLis]
+"""
 
-def masterMsg(rightHalfByte):
-    return [(0b1111 & rightHalfByte)<<4]
-
-def isMasterMsg(byte):
-    return (byte & (0b1111<<4))
-
-def transferLis(outLis,spi):
-    # outLis should be the vector of outgoing bytes, not yet left shifted
-    # returns
-    # [lis of bytes received, after re-assembly,correctedCount]
-    responseLis = []
-    correctedCount = 0
-    # ignore first reply
-    spi.xfer(masterMsg(outLis[0]),frequency,afterXferDelay)
-    time.sleep(pause)
-    
-    for outByte in outLis[1:]:
-        responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
-        while(isMasterMsg(responseLis[-1])):
-            correctedCount +=1
-            print('Error Corrected :',correctedCount)
-            responseLis = responseLis[:-1]
-            responseLis += spi.xfer(masterMsg(outByte),frequency,afterXferDelay)
-            # here we have a good value in r1
-        time.sleep(pause)
-    # here we have the response list filled with good values
-
-    resLis = []
-    for i in range(0, len(responseLis), 2):
-        resLis += [responseLis[i]<<4 | responseLis[i+1]]
-    return resLis,correctedCount
-
-def resonseToBeEnqueued(type):
-    return type == s_payload_t
-
-def getBid(currentResponseLis):
-    return currentResponseLis[1]
-
-bid = [-1]
-
-def doOneCom(type,spi,q,clearTheAir=False,printResults=False):
-    global bid
-    #print('Processing Query :',type)
-    outVec = getOutVec(type,typeDict[type][0])
-    transferCount  = 0
-    errorCount = 0
-    correctedCount = 0
-    lastResponseLis = [] 
-    init = False
-    saidEnq = False
-    # first one is ignored, just to clear the air!
-    if clearTheAir:
-        pass
-    #transferLis(outVec,spi)
-    moreDataComing = True
-    try:
-        while moreDataComing:
-            #input()
-            [currentResponseLis,correctedInc] = transferLis(outVec,spi)
-            transferCount+= 1  # note we are now counting transfers not bytes
-            correctedCount += correctedInc
-
-            currentResponseLis = unpackStruct(typeDict[type][1],packNbytes(currentResponseLis))
-            #currentResponse = bytes2float(currentResponseLis)
-            #print(currentResponseLis)
-            #input()
-
-            if printResults:
-                show(transferCount,#errorCount,correctedCount,
-                     currentResponseLis,
-                     type)
-            #input()
-            """
-            tell cannot work with query response
-            [init, errorCount,lastResponseLis] = tell(init,
-                                                      transferCount,
-                                                      errorCount,
-                                                      currentResponseLis,
-                                                      lastResponseLis,
-                                                      type)
-           
-            """
-            nullReturn = isNullReturn(currentResponseLis)
-            enQResponse = resonseToBeEnqueued(type)
-            
-            if not nullReturn and enQResponse:
-                if not saidEnq:
-                    #print('***** Enqueueing...')
-                    saidEnq = True
-                q.put(list(currentResponseLis) + bid)
-            elif type == s_bid_t:
-                bid[0] = getBid(currentResponseLis)
-                print('BID set :', bid), 
-                
-            moreDataComing = not nullReturn and enQResponse
-                 
-    except KeyboardInterrupt:
-        print('\nType of Transfers :', type)
-        print(  'Nb Transfers      :','{:,}'.format(transferCount).replace(',',' '))
-        print(  'Nb Errors         :',errorCount)
-        print(  'percent           :',100*errorCount/transferCount)
-        print(  'Corrected         :',correctedCount)
-        print('\nbye...')
-        raise 
-    #print('Sleeping :',typeDict[type][2])
-    time.sleep(typeDict[type][2])
-    
-
-def go(typeLis,q):
-    """ opens SPI
-    sends each elt in typeLis as per:
-    [0] : clearTheAir=True, printResults=True
-    [1] : clearTheAir=False, printResults=True
-    loop indefinitely:
-      [2] : use default values for clearTheAir and printResults
-    on exit, closes spi
-    """
-    spi = spidev.SpiDev()
-    spi.open(channel,device)
-    try:
-        print('Polling :',type2NameDict[typeLis[0]])
-        doOneCom(typeLis[0],spi,q,True,False)
-        print('Polling :',type2NameDict[typeLis[1]])
-        doOneCom(typeLis[1],spi,q,False,False)
-        count = 1
-        while True:
-            if (count%pollDisplayIterations == 0):
-                print(count, ': Polling :',type2NameDict[typeLis[2]])
-            count +=1                
-            doOneCom(typeLis[2],spi,q)
-    finally:
-        spi.close()
