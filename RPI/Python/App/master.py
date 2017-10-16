42eb871db1f07f132cdacfa803446e57665d1536
gratefulfrog <gratefulfrog@gmail.com>
2017-10-16 21:13:23 +0200

    documentated master.py

--- RPI/Python/App/master.py
+++ RPI/Python/App/master.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # example call
-# ./master.py s_init_t s s_payload_t
+# ./master.py 0 1 s_init_t s s_payload_t
 
 """ some results
 
@@ -39,6 +39,24 @@ fileLockDict = {}
 
 class WriterThread(threading.Thread):
     def __init__(self, name, q, lock, dataDir,syncTimeFunc):
+        """
+        Consumer class instanciation:
+        The Writer thread implements the consumer in the producer/consumer paradigm.
+        The number of threads created is not known by the individual threads. They
+        simply pop things off the shared thread-safe queue, extract the data, and
+        write to the correct data (csv) file, creating it and the data directory if needed.
+        The file names for the data files are created from the data elts themselves, in combination
+        with the information obtained by callingthe syncTimeFunc provided as argument.
+        Data file names are used as keys in the shared fileLockDict, whre the values are semaphores
+        ensuring unique access to each file.
+        @param self
+        @param name a string naming the thread, legacy but left to allow for easier debugging
+        @param q the thread-safe q which will be popped to get data
+        @param lock the semaphore ensure unique access to the fileLockDict
+        @param dataDir the path to the data directory
+        @param syncTimeFunc a function that will be called to get the synch time to be used in naming the
+        data files.
+        """
         threading.Thread.__init__(self)
         self.name = name
         self.q = q
@@ -52,6 +70,11 @@ class WriterThread(threading.Thread):
         self.getSynchTimeFunc = syncTimeFunc
 
     def getFormattedRow(self,row):
+        """
+        Formats a row by rounding the float values to 4 decimals
+        @param row the data row,
+        @return the row as a list ready to be written to the csv data file.
+        """
         row[2]= round(row[2],4)
         return row[1:3]
 
@@ -65,10 +88,22 @@ class WriterThread(threading.Thread):
             print('created file:',outFile)
 
     def decodeADCCID(self,coded):
+        """
+        Decodes the ADC and Channel values which were encoded onto a single byte such that
+        the top 4 bits are the ADC id, and the lower four are the channel id.
+        @param coded a single byte containing the encoded values
+        @return a list as decoded into [ADC_ID, Channel_ID]
+        """
         maskR = 0b1111
         return [((coded>>4)& 0b1111), coded & 0b1111]
             
     def getFile(self,row):
+        """
+        the filename is computed, then the dictLock is acquired to access the fileLockDict and
+        get a lock for the data file, creating the lock if needed.
+        the dictLock is released and the fileLock is aquire before returning
+        @param row the data to be used to get the file name
+        """
         [adcID,chID] = self.decodeADCCID(row[0])
         filename = self.dataDir + '/{bid}_{adc}_{cid}_{syc}.csv'.format(bid=row[3],adc=adcID,cid=chID,syc=self.getSynchTimeFunc())
         self.dictLock.acquire()
@@ -84,9 +119,19 @@ class WriterThread(threading.Thread):
         return filename 
 
     def releaseFile(self,filename):
+        """
+        Simply releases the fileLock
+        """
         self.fileLock.release()
 
     def do_work(self,thing):
+        """
+        This method handles the 'consuming' of the thing popped from the queue.
+        After obtaining a lock for the appropriate data file, a csv row is written,
+        the lock is released.
+        @param the object popped from the queue, in our case a list of length 4:
+        ADCCID,Timestamp,value, Board_ID
+        """
         filename = self.getFile(thing)
         #print('writing a row',thing)
         with open(filename, 'a', newline='') as csvfile:
@@ -98,7 +143,8 @@ class WriterThread(threading.Thread):
 
     def run(self):
         """
-        Thread run method. 
+        Thread run method. pops the queue, sends the popped thing to be consumed.
+        if a None value is popped, the method exits properply and the thread ends.
         """
         try:
             while True:
@@ -116,22 +162,44 @@ class WriterThread(threading.Thread):
 ###### Master Class CODE ##################
 
 class Master:
-    def __init__(self, nbThreads=3):
-        self.dataDir = './DATA'
+    def __init__(self, nbThreads=3,dataDir='./DATA'):
+        """
+        Constructor for class Master, implements the start and Producer part of
+        the Producer/Consumer paradigm for parallel processing. 
+        At instanciation, 
+        a FIFO, thread-safe queue is created which will be where the producer puts
+        his production and where the consumers get it.
+        A semaphore (threading.Lock) object is created to ensure unicity of access
+        the the fileLockDict used by all the consumer threads.
+        An insnce of the spiCommsMgr it created.
+        Finally the conusmer threads are created.
+        @param self,
+        @param nbThreads defaults to the initializer
+        @param dataDir default value provided, the directory where the conusmers
+        will write the csv files.
+        """
+        self.dataDir = dataDir
         self.q = queue.Queue()
         self.lock = threading.Lock()
         self.spiCommsMgr = comms.CommsMgr(self.q)
         self.createThreads(nbThreads,self.q,self.lock)
         
-    def createThreads(self,num,que,lok):
+    def createThreads(self,num):
+        """
+        Creates the number of consumer threads according to the argument.
+        @param num the number of threads to create
+        """
         self.threads = []
         for i in range(num):
             name='WriterThread-' + str(i)
-            t = WriterThread(name,que,lok,'./DATA',self.spiCommsMgr.getSynchTime)
+            t = WriterThread(name,self.q,self.lock,self.dataDir,self.spiCommsMgr.getSynchTime)
             t.start()
             self.threads.append(t)
 
     def stopAll(self):
+        """ 
+        Called at the end of a run, it allows all the consumer threads to exit properly
+        """
         # block until all tasks are done
         self.q.join()
 
@@ -142,6 +210,12 @@ class Master:
             t.join()
 
     def run(self,typeLis):
+        """
+        called to start a data run, after the consumer threads have been started.
+        will display the elapsed time on exit.
+        To exit, ctrl-c will be handled properly.
+        @param tyleLis is a list of the messages to pass on the commsMgr loop. These are values not strings.
+        """
         t = time.time()
         try:
             self.spiCommsMgr.loop(typeLis)
