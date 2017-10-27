fb653e566245bd8300dbb3faadc6448d7c1feb68
gratefulfrog <gratefulfrog@gmail.com>
2017-10-27 21:35:13 +0200

    experiments with SLIP OSC over USB serial

---
+++ RPI/Python/Old/OSC_SLIP_TESTS/Arduino_OscSlip_00/Arduino_OscSlip_00.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#include "stdDefs.h"
+#include "MsgMgr.h"
+
+MsgMgr *comms;
+
+u8u32f_struct s = {0,10,100.0};
+
+char handshakeChar = '|';
+
+void handShake(){
+  while (Serial.available() <=0){
+  }
+}
+
+void setup() {
+  Serial.begin(2000000);
+  while(!Serial);
+  handShake();
+  
+  comms = new MsgMgr();
+}
+
+void loop() {
+  static int i=0;
+  //comms->send(i++);
+  
+  comms->send(&s);
+  s.u8++;
+  s.u32+=10;
+  s.f+=0.01;
+  
+  delay(2);
+}
