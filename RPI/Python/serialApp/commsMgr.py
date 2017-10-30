f58299db42356a325a3723d3ce7ec49a20869466
gratefulfrog <gratefulfrog@gmail.com>
2017-10-30 18:16:32 +0100

    working pure serial version seems really fast

--- RPI/Python/serialApp/commsMgr.py
+++ RPI/Python/serialApp/commsMgr.py
@@ -9,7 +9,8 @@ import threading
 import itertools
 
 #### for debugging, will display a heartbeat message after this number of polls
-pollDisplayIterations = 32000 # 1 min (PC) , => = 1,920,000/hour
+pollDisplayIterations = 2000000 #= +/-1 hour # 32000 # = 1 min
+#pollDisplayIterations = 32000 # = 1 min 
 
 def packNbytes(bytes):
     """ 
